d9a82e2f9d14e7e3656465ab8d8f25531a947047
Nurul577 <nurul959595islam@gmail.com>
2025-11-16 01:54:33 +0530

    Add changelog and author information

--- simple-interest.sh
+++ simple-interest.sh
@@ -1,1 +1,13 @@
-
+##########################################
+# Author(s)
+# Nurul Islam
+#
+# Changelog
+# Date         Version   Changed by        Change Description
+# 2021-12-29   0.1       Upkar Lidder      Initial instructions for the final project
+# 2022-01-13   0.2       Alison Woolford   Updates to instructions for learner clarity
+# 2022-01-20   1.0       Upkar Lidder      Final version
+# 2022-02-25   1.0       CF                Fixed URLs
+# 2023-03-04   1.1       Lavanya Rajalingam Updated new SN Logo
+# 2023-04-04   1.2       Lavanya Rajalingam Corrected number of URL’s in Checklist section
+##########################################
